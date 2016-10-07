808a75d4b427cec1d8e14ae7a4515b39c291bd8b
OhtaS <t2531011@eng.kitakyu-u.ac.jp>
2016-10-07 21:41:15 +0900

    クレーンの操作をCraneStateMachineに委託

--- Assets/Script/Crane/Crane.cs
+++ Assets/Script/Crane/Crane.cs
@@ -27,13 +27,11 @@ namespace Crane{
 		Vector3 default_position;
 		Vector3 correctBox_position;
 		Vector3 incorrectBox_position;
-		public static State state;
-		public Answer player_answer;
+		public State state;
 		public bool isCatched;
 
 		void Start(){
 			state = State.Ready;
-			player_answer = Answer.Unanswered;
 			isCatched = false;
 			default_position = transform.position;
 			arm_l = transform.GetChild(0);
@@ -46,70 +44,7 @@ namespace Crane{
 			incorrectBox_position = GameObject.Find("IncorrectBox").transform.localPosition;
 		}
 
-		void Update(){
-			PlayMultipleChoice();
-		}
-
-		void PlayMultipleChoice(){
-			if (Input.GetKey(KeyCode.Space) && state == State.MoveX){
-				state = State.Open;
-			}
-			if (Input.GetKeyUp(KeyCode.LeftArrow)){
-				player_answer = Answer.Correct;
-			} else if (Input.GetKeyUp(KeyCode.RightArrow)){
-				player_answer = Answer.Incorrect;
-			}
-
-			switch(state){
-				case State.Ready:
-					CloseArms(state);
-				break;
-
-				case State.Open:
-					OpenArms(State.Fall);
-				break;
-
-				case State.Fall:
-					Fall();
-				break;
-
-				case State.Close:
-					CloseArms(State.Rise);
-				break;
-
-				case State.Rise:
-					Rise();
-				break;
-
-				case State.WaitingAnswer:
-					GameObject.Find("Ochimusha").GetComponent<Ochimusha>().Question();
-					if (player_answer == Answer.Correct){
-						MoveCorrect();
-					} else if (player_answer == Answer.Incorrect){
-						MoveIncorrect();
-					}
-				break;
-
-				case State.WaitingJudgement:
-					if (GameObject.Find("Ochimusha").GetComponent<Ochimusha>().Judge(player_answer) == false){
-						GameObject.Find("Ochimusha").GetComponent<Ochimusha>().ResponeIjin();
-					} else{
-					}
-					if (SceneManager.GetSceneByName("Question").isLoaded == true){
-						GameObject.Find("Button_left").GetComponent<BoxCollider2D>().enabled = true;
-						GameObject.Find("Button_right").GetComponent<BoxCollider2D>().enabled = true;
-						UnityEngine.SceneManagement.SceneManager.UnloadScene("Question");
-					}
-					state = State.Return;
-				break;
-
-				case State.Return:
-					ReturnToBase();
-				break;
-			}
-		}
-
-		void Fall(){
+		public void Fall(){
 			if (transform.position.y >= default_position.y - 0.5f){
 				transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
 			} else{
@@ -117,7 +52,7 @@ namespace Crane{
 			}
 		}
 
-		void CloseArms(State next_state){
+		public void CloseArms(State next_state){
 			if (Mathf.Abs(Mathf.DeltaAngle(arm_l.eulerAngles.z, close_angle_l.z)) < 0.1f && Mathf.Abs(Mathf.DeltaAngle(arm_r.eulerAngles.z,
 			                                                                                                           close_angle_r.z)) < 0.1f){
 				state = next_state;
@@ -127,7 +62,7 @@ namespace Crane{
 			}
 		}
 
-		void Rise(){
+		public void Rise(){
 			if (transform.position.y >= default_position.y){
 				if (isCatched == true){
 					state = State.WaitingAnswer;
@@ -139,7 +74,7 @@ namespace Crane{
 			}
 		}
 
-		void ReturnToBase(){
+		public void ReturnToBase(){
 			if (transform.position.x - default_position.x > 0.01){
 				transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
 			} else if (transform.position.x - default_position.x < -0.01){
@@ -149,7 +84,7 @@ namespace Crane{
 			}
 		}
 
-		void OpenArms(State next_state){
+		public void OpenArms(State next_state){
 			if (Mathf.DeltaAngle(arm_l.eulerAngles.z, open_angle_l.z) < 0.1f && Mathf.DeltaAngle(arm_r.eulerAngles.z,
 			                                                                                     open_angle_r.z) < 0.1f){
 				state = next_state;
@@ -183,7 +118,7 @@ namespace Crane{
 			state = State.Open;
 		}
 
-		void MoveCorrect(){
+		public void MoveCorrect(){
 			if (transform.localPosition.x > correctBox_position.x){
 				transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
 			} else{
@@ -191,12 +126,34 @@ namespace Crane{
 			}
 		}
 
-		void MoveIncorrect(){
+		public void MoveIncorrect(){
 			if (transform.localPosition.x < incorrectBox_position.x){
 				transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
 			} else{
 				OpenArms(State.WaitingJudgement);
 			}
+		}
+
+		public void WaitAnswer(Answer player_answer){
+			GameObject.Find("Ochimusha").GetComponent<Ochimusha>().Question();
+			if (player_answer == Answer.Correct){
+				MoveCorrect();
+			} else if (player_answer == Answer.Incorrect){
+				MoveIncorrect();
+			}
+		}
+
+		public void WaitJudgement(Answer player_answer){
+			if (GameObject.Find("Ochimusha").GetComponent<Ochimusha>().Judge(player_answer) == false){
+				GameObject.Find("Ochimusha").GetComponent<Ochimusha>().ResponeIjin();
+			} else{
+			}
+			if (SceneManager.GetSceneByName("Question").isLoaded == true){
+				GameObject.Find("Button_left").GetComponent<BoxCollider2D>().enabled = true;
+				GameObject.Find("Button_right").GetComponent<BoxCollider2D>().enabled = true;
+				UnityEngine.SceneManagement.SceneManager.UnloadScene("Question");
+			}
+			state = State.Return;
 		}
 	}
 }
---
+++ Assets/Script/Crane/CraneStateMachine.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ijin;
+
+namespace Crane{
+	public class CraneStateMachine : MonoBehaviour{
+		Crane crane;
+		Answer player_answer;
+		// Use this for initialization
+		void Start(){
+			crane = gameObject.GetComponent<Crane>();
+			player_answer = Answer.Unanswered;
+		}
+	
+		// Update is called once per frame
+		void Update(){
+			if (Input.GetKey(KeyCode.Space) && crane.state == State.MoveX){
+				crane.state = State.Open;
+			}
+			if (player_answer == Answer.Unanswered && Input.GetKeyUp(KeyCode.LeftArrow)){
+				player_answer = Answer.Correct;
+			} else if (player_answer == Answer.Unanswered && Input.GetKeyUp(KeyCode.RightArrow)){
+				player_answer = Answer.Incorrect;
+			}
+
+			switch(crane.state){
+				case State.Ready:
+					player_answer = Answer.Unanswered;
+					crane.CloseArms(crane.state);
+				break;
+
+				case State.Open:
+					crane.OpenArms(State.Fall);
+				break;
+
+				case State.Fall:
+					crane.Fall();
+				break;
+
+				case State.Close:
+					crane.CloseArms(State.Rise);
+				break;
+
+				case State.Rise:
+					crane.Rise();
+				break;
+
+				case State.WaitingAnswer:
+					crane.WaitAnswer(player_answer);
+				break;
+
+				case State.WaitingJudgement:
+					crane.WaitJudgement(player_answer);
+				break;
+
+				case State.Return:
+					crane.ReturnToBase();
+				break;
+			}
+		}
+	}
+}
