5dd03db39589e280bdeb6019384e32298fdd1ed7
OhtaS <t2531011@eng.kitakyu-u.ac.jp>
2016-11-27 17:02:45 +0900

    台選択シーンの作成 	クレーンシーンに遷移 図鑑シーンの作成 	タイトルシーンに遷移

---
+++ Assets/Script/SelectStand/DecisionButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SelectStand{
+	public class DecisionButton : AbstractButton{
+		protected override void ActionByPushed(){
+			StartCoroutine(Common.MySceneManager.LoadScene("MultipleChoice2"));
+		}
+	}
+}
