61eb94f6be6ed1bc4aa49f3f2c7900a76469e45e
OhtaS <t2531011@eng.kitakyu-u.ac.jp>
2016-11-27 16:39:45 +0900

    タイトルシーンの作成 	台選択シーンに遷移 	図鑑シーンに遷移

---
+++ Assets/Script/Common/MySceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Common{
+	public static class MySceneManager{
+		static bool isLoading = false;
+
+		/// <summary>
+		/// シーンのロード。ロードが完了するまで待機。
+		/// </summary>
+		/// <returns>The scene.</returns>
+		/// <param name="sceneName">Scene name.</param>
+		public static IEnumerator LoadScene(string sceneName){
+			if (isLoading){
+				yield break;
+			}
+			isLoading = true;
+			Scene currentScene = SceneManager.GetActiveScene();
+			yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+			isLoading = false;
+			SceneManager.UnloadScene(currentScene);
+		}
+	}
+}
